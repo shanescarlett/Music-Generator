40aba7223faf726778257a683718196ca82c54cf
Shane Scarlett <shane.choo+github@gmail.com>
2018-10-08 09:27:52 +0900

    Read files

--- Main.py
+++ Main.py
@@ -2,6 +2,13 @@ import glob
 import music21
 from music21 import converter, instrument, note, chord
 
+notes = []
+folder = glob.glob("C:/Users/Main/Documents/Data/Chopin/*.mid")
+count = 1
+for file in folder:
+	print('Reading file %d of %d: %s' % (count, len(folder), repr(file)))
+	score: music21.stream.Score = converter.parse(file)
+
 
 
 print('End')
