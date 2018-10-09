f190986ad8c550a34f70e16c63330ce16de7a452
Shane Scarlett <shane.choo+github@gmail.com>
2018-10-09 09:28:08 +0900

    Initial parsing

--- Main.py
+++ Main.py
@@ -9,6 +9,10 @@ for file in folder:
 	print('Reading file %d of %d: %s' % (count, len(folder), repr(file)))
 	score: music21.stream.Score = converter.parse(file)
 
-
+	# Transpose to common key
+	key: music21.key.Key = score.analyze('key')
+	intervalToC: music21.interval.Interval = music21.interval.Interval(key.tonic, music21.pitch.Pitch('C'))
+	score.transpose(intervalToC, inPlace = True)
+	count += 1
 
 print('End')
