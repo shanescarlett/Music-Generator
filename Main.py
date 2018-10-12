f5baa0bfb913fd6ece6b03b134db68b268fa82eb
Shane Scarlett <shane.choo+github@gmail.com>
2018-10-12 10:36:20 +0900

    Functionalise

--- Main.py
+++ Main.py
@@ -2,18 +2,14 @@ import glob
 import music21
 from music21 import converter, instrument, note, chord
 
-notesAndChords = []
-folder = glob.glob("C:/Users/Main/Documents/Data/Chopin/*.mid")
-count = 1
-for file in folder:
-	print('Reading file %d of %d: %s' % (count, len(folder), repr(file)))
-	score: music21.stream.Score = converter.parse(file)
+def readAndParseFile(path):
+	notesAndChords = []
+	score: music21.stream.Score = converter.parse(path)
 
 	# Transpose to common key
 	key: music21.key.Key = score.analyze('key')
 	intervalToC: music21.interval.Interval = music21.interval.Interval(key.tonic, music21.pitch.Pitch('C'))
 	score.transpose(intervalToC, inPlace = True)
-	count += 1
 
 	parts = music21.instrument.partitionByInstrument(score)
 	if parts:  # file has instrument parts
@@ -27,4 +23,16 @@ for file in folder:
 		elif isinstance(element, chord.Chord):
 			notesAndChords.append(element)
 
+	return notesAndChords
+
+def main():
+	count = 1
+	notesAndChords = []
+	folder = glob.glob("C:/Users/Main/Documents/Data/Chopin/*.mid")
+	for file in folder:
+		print('Reading file %d of %d: %s' % (count, len(folder), repr(file)))
+		notesAndChords.append(readAndParseFile(file))
+		count += 1
+
+main()
 print('End')
