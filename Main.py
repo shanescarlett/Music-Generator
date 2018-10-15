ef3fde0db52b2122cdd43d91a49248a32015c615
Shane Scarlett <shane.choo+github@gmail.com>
2018-10-15 16:46:03 +0900

    Minor cleanup

--- Main.py
+++ Main.py
@@ -2,7 +2,6 @@ import glob
 import music21
 from music21 import converter, instrument, note, chord
 import numpy as np
-import sklearn.preprocessing
 
 
 def getAllNotesPlayingAt(offsetStart, offsetEnd, stream):
